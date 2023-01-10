e2014978d48ff7c151780bf7fa5085f99789eb14
Tomas Persson <hypotesfabriken@gmail.com>
2023-01-10 11:46:56 +0100

    Add more element types to check

--- scan-site.js
+++ scan-site.js
@@ -4,14 +4,27 @@ Know issues:
 Conotent security policys on the site might stop the execution (this is a good thing).
 */
 function extractHostname(url) {
+
+    if(url[0] == undefined) {
+        console.log("url undefined!");
+        console.log(url[0]);
+        console.log(url[1]);
+        return "noVal";
+    } 
+    if(url[1] == "LINK" && url[0].search("data") != -1) {
+        console.log("exit 2");
+        console.log(url[0]);
+        return "noVal";
+    }
+
     var hostname;
     //find & remove protocol (http, ftp, etc.) and get hostname
 
-    if (url.indexOf("//") > -1) {
-        hostname = url.split('/')[2];
+    if (url[0].indexOf("//") > -1) {
+        hostname = url[0].split('/')[2];
     }
     else {
-        hostname = url.split('/')[0];
+        hostname = url[0].split('/')[0];
     }
 
     //find & remove port number
@@ -32,18 +45,48 @@ function fullSite(domain, country, script) {
   this.script = script;
 }
 
+var urls = [];
+var tags = document.getElementsByTagName("*");
+const doNotCheck = ["SVG", "SVGAnimatedString", "SPAN", "A", "use"];
+var siteHostName =  window.location.host;
+for(i=0;i<tags.length;i++) {
+    if(tags[i].nodeName == "IMG" && tags[i].src.search("data") == -1) {
+        urls.push([tags[i].href,tags[i].nodeName]);
+        console.log(tags[i].href);
+        console.log(tags[i].nodeName);
+        console.log(tags[i].src.search("data"));
+    }
+    else if(tags[i].nodeName == "IMG" && tags[i].src.search("data") != -1) {
+                //Do nothing
+    }
+    else if(doNotCheck.includes(tags[i].nodeName) ) {
+        //Do nothing
+    }
+    else if(tags[i].href != undefined && tags[i].href != "" && tags[i].href.search(siteHostName) == -1) {
+        urls.push([tags[i].href,tags[i].nodeName]);
+        console.log(tags[i].href);
+        console.log(tags[i].nodeName);
+    }
+    else if(tags[i].src != undefined && tags[i].src != ""  && tags[i].src.search(siteHostName) == -1) {
+       urls.push(tags[i].src);
+       urls.push([tags[i].src,tags[i].nodeName]);
+       console.log(tags[i].src);
+       console.log(tags[i].nodeName);
 
-var scripts = document.querySelectorAll("script[src]");
-//var scripts = document.querySelectorAll("link[href]");
+    }
+}
+
 
 var fullDomains = [];
 var domains = [];
-for (i=0;i<scripts.length;i++) {
-    var hstName = extractHostname(scripts[i].src);
+for (i=0;i<urls.length;i++) {
+    var hstName = extractHostname(urls[i]);
     if(window.location.host.includes(hstName)) {
        //console.log("do nothing" );
     }
-
+    else if(hstName == "noVal") {
+       //console.log("do nothing" );
+    }
     else {
         if(domains.includes(hstName)) {
             //do nothing
@@ -58,14 +101,15 @@ for (i=0;i<scripts.length;i++) {
            var json2 = await response2.text();
 
            domains.push(new site(hstName, json2));
-           fullDomains.push(new fullSite(hstName, json2, scripts[i].src));
+           fullDomains.push(new fullSite(hstName, json2, urls[i][0]));
           }
         }
     }
 }
+
 console.log("A list of all domains");
 console.table(domains);
-console.log("A list of all scripts");
+console.log("A list of all urls");
 console.table(fullDomains);
 
 
